69493234755758b504e0020bd8f50757ecf545e6
evan <evanstock@gmail.com>
2025-05-06 21:05:25 -0500

    flake update

--- hosts/starchy/configuration.nix
+++ hosts/starchy/configuration.nix
@@ -96,7 +96,7 @@
   };
 
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  #xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
   xdg.portal.config.common.default = "*";
 
   # Allow unfree packages
